bffef2e70b69014122973451d4992765db1626eb
lucas.gimenez <l.adrianobf@gmail.com>
2023-07-30 10:28:34 -0300

    implementando as classes

---
+++ src/Cliente.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cliente {
+
+    private String nome;
+    private List<Endereco> enderecos;
+
+    public Cliente(String nome) {
+        this.nome = nome;
+        this.enderecos = new ArrayList<>();
+    }
+
+    public void addEndereco(Endereco endereco) {
+        this.enderecos.add(endereco);
+    }
+
+    public void removeEndereco(Endereco endereco) {
+        this.enderecos.remove(endereco);
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+}
+
