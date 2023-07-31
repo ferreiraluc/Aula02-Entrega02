8f56f9b49641ff60eef06bbcfd7c3b71dc7e5268
lucas.gimenez <l.adrianobf@gmail.com>
2023-07-30 21:09:15 -0300

    organizando o codigo

--- src/MenuCadastro.java
+++ src/MenuCadastro.java
@@ -12,71 +12,21 @@ public class MenuCadastro {
         int opcao;
 
         do {
-            System.out.print("##------------Menu-----------##\n\n");
-            System.out.print("|-----------------------------|\n");
-            System.out.print("| Opção 1 - Clientes |\n");
-            System.out.print("| Opção 2 - Pedidos   |\n");
-            System.out.print("| Opção 3 -      |\n");
-            System.out.print("| Opção 4 -    |\n");
-            System.out.print("| Opção 5 - Sair              |\n");
-            System.out.print("|-----------------------------|\n");
-            System.out.print("Digite uma opção: ");
+            mostrarMenu();
             opcao = scanner.nextInt();
 
             switch (opcao) {
                 case 1:
-                    System.out.println("\n##--- Clientes ---##");
-                    System.out.println("|-----------------------------|");
-                    System.out.println("| 1 - Cadastrar Cliente   |");
-                    System.out.println("| 2 - Remover Cliente    |");
-                    System.out.println("| 3 - Editar Cliente    |");
-                    System.out.println("| 4 - Listar Clientes    |");
-                    System.out.println("|-----------------------------|");
-                    System.out.print("Digite uma opção: ");
-                    int opcaoClientes = scanner.nextInt();
-                    switch (opcaoClientes) {
-                        case 1:
-                            RealizarCadastro rc = new RealizarCadastro();
-                            rc.RealizarCadastro(clientes, scanner);
-                            break;
-                        case 2:
-                            RealizarCadastro.removerCliente(clientes, scanner);
-                            break;
-                        case 3:
-                            RealizarCadastro.editarEnderecoCliente(clientes, scanner);
-                            break;
-                        case 4:
-                            Relatorio.gerarRelatorioClientes(clientes, scanner);
-                            break;
-                        default:
-                            System.out.println("Opção inválida!");
-                            break;
-                    }
+                    menuClientes(clientes, scanner);
                     break;
                 case 2:
                     RealizarPedido.realizarPedido(clientes, pedidos, scanner);
                     break;
                 case 3:
-                    System.out.println("\n##--- Menu de Relatórios ---##");
-                    System.out.println("|-----------------------------|");
-                    System.out.println("| 1 - Relatório de Clientes   |");
-                    System.out.println("| 2 - Relatório de Pedidos    |");
-                    System.out.println("|-----------------------------|");
-                    System.out.print("Digite uma opção: ");
-                    int opcaoRelatorio = scanner.nextInt();
-                    switch (opcaoRelatorio) {
-                        case 1:
-                            Relatorio.gerarRelatorioClientes(clientes, scanner);
-                            break;
-                        case 2:
-                            Relatorio.gerarRelatorioPedidos(pedidos);
-                            break;
-                        default:
-                            System.out.println("Opção inválida!");
-                            break;
-                    }
+                    menuRelatorios(clientes, pedidos, scanner);
                     break;
                 case 4:
+                    // EMITIR TXT
                     break;
                 case 5:
                     System.out.println("Saindo...");
@@ -88,11 +38,69 @@ public class MenuCadastro {
         } while (opcao != 5);
 
         scanner.close();
-
-
-
     }
 
+    public static void mostrarMenu() {
+        System.out.print("##------------Menu-----------##\n\n");
+        System.out.print("|-----------------------------|\n");
+        System.out.print("| Opção 1 - Clientes          |\n");
+        System.out.print("| Opção 2 - Pedidos           |\n");
+        System.out.print("| Opção 3 - Relatórios        |\n");
+        System.out.print("| Opção 4 - EMITIR TXT                |\n");
+        System.out.print("| Opção 5 - Sair              |\n");
+        System.out.print("|-----------------------------|\n");
+        System.out.print("Digite uma opção: ");
+    }
+
+    public static void menuClientes(List<Cliente> clientes, Scanner scanner) {
+        System.out.println("\n##--- Clientes ---##");
+        System.out.println("|-----------------------------|");
+        System.out.println("| 1 - Cadastrar Cliente       |");
+        System.out.println("| 2 - Remover Cliente         |");
+        System.out.println("| 3 - Editar Cliente          |");
+        System.out.println("| 4 - Listar Clientes         |");
+        System.out.println("|-----------------------------|");
+        System.out.print("Digite uma opção: ");
+        int opcaoClientes = scanner.nextInt();
+        switch (opcaoClientes) {
+            case 1:
+                RealizarCadastro rc = new RealizarCadastro();
+                rc.RealizarCadastro(clientes, scanner);
+                break;
+            case 2:
+                RealizarCadastro.removerCliente(clientes, scanner);
+                break;
+            case 3:
+                Relatorio.gerarRelatorioClientes(clientes, scanner);
+                RealizarCadastro.editarEnderecoCliente(clientes, scanner);
+                break;
+            case 4:
+                Relatorio.gerarRelatorioClientes(clientes, scanner);
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
+    }
+
+    public static void menuRelatorios(List<Cliente> clientes, List<Pedido> pedidos, Scanner scanner) {
+        System.out.println("\n##--- Menu de Relatórios ---##");
+        System.out.println("|-----------------------------|");
+        System.out.println("| 1 - Relatório de Clientes   |");
+        System.out.println("| 2 - Relatório de Pedidos    |");
+        System.out.println("|-----------------------------|");
+        System.out.print("Digite uma opção: ");
+        int opcaoRelatorio = scanner.nextInt();
+        switch (opcaoRelatorio) {
+            case 1:
+                Relatorio.gerarRelatorioClientes(clientes, scanner);
+                break;
+            case 2:
+                Relatorio.gerarRelatorioPedidos(pedidos);
+                break;
+            default:
+                System.out.println("Opção inválida!");
+                break;
+        }
+    }
 }
-
-
